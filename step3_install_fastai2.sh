d797ffa7732a64370e4e3ebf50a0f8bc819a7c49
streicherlouw <streicher@streicher.dk>
2020-05-15 19:39:10 +1000

    Update step3_install_fastai2.sh

--- step3_install_fastai2.sh
+++ step3_install_fastai2.sh
@@ -24,15 +24,6 @@ echo $PW | sudo -k --stdin apt -y update
 echo $PW | sudo -k --stdin apt -y upgrade
 echo $PW | sudo -k --stdin apt -y autoremove
 
-# Create a virtual environment and activate it
-echo $PW | sudo -k --stdin apt install -y python3-venv
-cd ~/
-python3 -m venv ~/python-envs/fastai
-source ~/python-envs/fastai/bin/activate
-
-echo $PW | sudo -k --stdin apt install -y python3-pip
-pip3 install wheel
-
 # Install MAGMA from source
 # Since fastai requires pytorch to be compiled MAGMA, MAGMA needs to be installed first
 # The authors of MAGMA does not offer binary builds, so it needs to be compiled from source
@@ -43,7 +34,7 @@ echo "Start installation of MAGMA at: $now"
 echo $PW | sudo -k --stdin apt install -y libopenblas-dev
 echo $PW | sudo -k --stdin apt install -y gfortran
 echo $PW | sudo -k --stdin apt install -y cmake
-echo $PW | sudo -k --stdin apt install -y libnuma-dev #Used for pytorch later
+# echo $PW | sudo -k --stdin apt install -y libnuma-dev #Used for pytorch later
 wget http://icl.utk.edu/projectsfiles/magma/downloads/magma-2.5.3.tar.gz
 tar -xf magma-2.5.3.tar.gz
 # Magma needs a make.inc file to tell it which Nvidia architectures to compile for and where to find the blas libraries
@@ -56,9 +47,6 @@ export PATH=$PATH:/usr/local/cuda-10.2/bin
 make
 echo $PW | sudo -k --stdin --preserve-env make install prefix=/usr/local/magma
 cd ~/
-
-now=`date`
-echo "Start installation of various library dependencies at: $now"
 
 # Install dependencies for fastai
 echo $PW | sudo -k --stdin apt install -y graphviz
@@ -76,6 +64,17 @@ echo $PW | sudo -k --stdin apt-get -y install libfreetype6-dev
 echo $PW | sudo -k --stdin apt install -y python-cffi
 echo $PW | sudo -k --stdin apt install -y libffi-dev
 echo $PW | sudo -k --stdin apt install -y libssl-dev
+
+# Create a virtual environment and activate it
+echo $PW | sudo -k --stdin apt install -y python3-venv
+cd ~/
+python3 -m venv ~/python-envs/fastai
+source ~/python-envs/fastai/bin/activate
+
+pip3 install wheel
+
+now=`date`
+echo "Start installation of various library dependencies at: $now"
 
 # Install dependencies for scipy and scikit-learn, torch, torchvision, jupyter notebook and fastai
 pip3 install cython
